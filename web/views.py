a0bcbf189cac0049d22bc1a53b6b09eb57bc03a8
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-05-31 00:12:57 +0900

    更新処理実装

--- web/views.py
+++ web/views.py
@@ -1,11 +1,11 @@
 from web import app, db
 from web.forms import BookForm
 from web.models import Book
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
-    books = Book.query.all()
+    books = Book.query.order_by(Book.date.desc()).all()
     return render_template('index.html', books=books)
 
 @app.route('/register', methods=['GET','POST'])
@@ -18,3 +18,26 @@ def register_book():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('register_book.html', form=form)
+
+@app.route('/<int:id>/update', methods=['GET','POST'])
+def update_book(id):
+    book = Book.query.get(id)
+
+    form = BookForm()
+
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.author = form.author.data
+        book.genre = form.genre.data
+        book.date = form.date.data
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    elif request.method == 'GET':
+        form.title.data = book.title
+        form.author.data = book.author
+        form.genre.data = book.genre
+        form.date.data = book.date
+
+    return render_template('each_book.html', form=form, id=id)
